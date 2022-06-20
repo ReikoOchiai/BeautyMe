84046fd0de3fd77d528e6a9ab1cc9b20d8e77ed9
ReikoOchiai <lissage.23@gmail.com>
2022-06-21 00:17:32 +1000

    point light has been added

--- react-three-fiber-project/src/App.js
+++ react-three-fiber-project/src/App.js
@@ -3,13 +3,13 @@ import "./App.scss";
 import { Canvas, useFrame } from "react-three-fiber";
 // import { Box } from "@react-three/drei";
 
-const SpinningMesh = ({ position, args }) => {
+const SpinningMesh = ({ position, args, color }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
     <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={args} />
-      <meshStandardMaterial attach="material" color="light pink" />
+      <meshStandardMaterial attach="material" color={color} />
     </mesh>
   );
 };
@@ -19,9 +19,19 @@ function App() {
     <>
       <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
         <ambientLight intensity={0.3} />
-        <SpinningMesh position={[0, 1, 0]} args={[2, 2, 2]} />
-        <SpinningMesh position={[-2, 1, -5]} args={[1, 1, 1]} />
-        <SpinningMesh position={[5, 1, -2]} args={[1, 1, 1]} />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="hotpink" />
+        <SpinningMesh
+          position={[-2, 1, -5]}
+          args={[1, 1, 2]}
+          color="lightpink"
+        />
+        <SpinningMesh
+          position={[5, 1, -2]}
+          args={[1, 1, 1]}
+          color="lightpink"
+        />
       </Canvas>
     </>
   );
