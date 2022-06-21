325cdba6c8b590795604ff9dc33bd9c2402c1cfd
ReikoOchiai <lissage.23@gmail.com>
2022-06-21 23:05:29 +1000

    File name changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,8 +19,20 @@ function App() {
     <>
       <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
         <ambientLight intensity={0.3} />
+        <directionalLight
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
         <pointLight position={[-10, 0, -20]} intensity={0.5} />
         <pointLight position={[0, -10, 0]} intensity={1.5} />
+
         <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="hotpink" />
         <SpinningMesh
           position={[-2, 1, -5]}
